8c8ea5004de9baec93d8ee1d8eaef105024e9984
Marco Sirabella <msirabel@gmail.com>
2017-03-16 17:30:17 -0400

    - Default make and setup

---
+++ src/setup.mk
@@ -0,0 +1,18 @@
+# uncomment these if running from default project location
+# Path to ChibiOS
+CHIBIOS    = include/ChibiOS
+# Path to convex
+CONVEX     = include/cortex
+
+PROJECT = robos
+BUILDDIR = build
+INCDIR = src
+# uncomment to use the optional code like the smart motor library
+#CONVEX_OPT  = yes
+
+# User C code files
+VEXUSERSRC = src/vexuser.c
+
+# Uncomment and add/modify user include files
+VEXUSERINC = src/
+
