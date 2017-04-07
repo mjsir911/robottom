399b0a1917646b82749ad1c6fb9600da80c0f1f0
Marco Sirabella <msirabel@gmail.com>
2017-04-06 23:13:14 -0400

    - Made workspace not static

--- src/meCompetitionControlSpoof.c
+++ src/meCompetitionControlSpoof.c
@@ -3,7 +3,7 @@
 /*-----------------------------------------------------------------------------*/
 /** @brief      Manual mode control                                            */
 /*-----------------------------------------------------------------------------*/
-static WORKING_AREA(waModeControl,64);
+WORKING_AREA(waModeControl,64);
 msg_t modeControl( void *arg ) {
   (void)arg;
   vexTaskRegister("modeControl");
