ec606959c7f4dfb2979c7254ca1eec1911b3f191
Marco Sirabella <msirabel@gmail.com>
2017-04-26 15:37:51 -0400

    - Simulated actual competition control

--- src/meCompetitionControlSpoof.c
+++ src/meCompetitionControlSpoof.c
@@ -7,16 +7,26 @@
 /*  https://www.vexforum.com/index.php/10069-convex-open-source-firmware-for-the-cortex/0?search=active+last+%3F+days
  *  */
 /*-----------------------------------------------------------------------------*/
+void setAutoOn(void) {
+  vexControllerCompetitionStateSet(kFlagAutonomousMode, kFlagAutonomousMode | kFlagAutonomousMode);
+}
+void setContOn(void) {
+  vexControllerCompetitionStateSet(0, kFlagAutonomousMode | kFlagDisabled); 
+}
+
+
 WORKING_AREA(waModeControl,64);
 msg_t modeControl( void *arg ) {
   (void)arg;
   vexTaskRegister("modeControl");
 
+  setAutoOn();
+  vexSleep(15 * 1000);
+  vexContOn();
   //vexControllerCompetitionStateSet(1, kFlagDisabled);
 
-  while (TRUE) { 
+
     //if (vexControllerGet(Ch3) > 0) {
-    if (true) {
       /*
       *   (value, mask)
       *   mask is which controller flag you're changing
@@ -25,15 +35,8 @@ msg_t modeControl( void *arg ) {
       *   0 is driver control mode
       *   1 is autonomous mode
       */
-      vexControllerCompetitionStateSet(0, kFlagAutonomousMode | kFlagDisabled); 
       // ABOVE ENABLES CONTROLLER MODE
-    }
-    else if (vexControllerGet(Ch3) < 0) {
-      vexControllerCompetitionStateSet(kFlagAutonomousMode, kFlagAutonomousMode | kFlagAutonomousMode);
       // ABOVE ENABLES AUTONOMOUS MODE
-    }
-    vexSleep(25);
-  }
 
   return (msg_t)0;
 
